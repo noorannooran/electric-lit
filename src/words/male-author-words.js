4fd8cca01ee14965346b7812d815d5b228e9f648
Nooran El-Sherif <nooran.elsherif@gmail.com>
2018-05-14 17:30:51 -0400

    entered words for male author description generator

--- src/words/male-author-words.js
+++ src/words/male-author-words.js
@@ -1,8 +1,23 @@
-const attr = []
-const adj = []
-const nouns = []
-const verbs1 = []
-const verbs2 = []
+const attr = ['boobs', 'a bust', 'a butt', 'lips', 'an ass', 'breasts', 'hair', 'eyes',
+  'a voice', 'curves', 'a rump', 'legs', 'a rear end', 'mammaries', 'jugs', 'calves',
+  'a badonkadonk', 'gams', 'knockers', 'a complexion', 'a pout', 'hooters', 'a booty', 'cheeks',
+  'thighs', 'tresses']
+const adj = ['silken', 'creamy', 'shiny', 'ripe', 'juicy', 'middle-aged', 'rippling', 'wrinkled',
+  'luscious', 'frigid', 'voluptuous', 'expensive', 'plump', 'bountiful', 'tempestuous', 'witherered',
+  'haughty', 'shrill', 'fat', 'dewy', 'soft', 'wiry', 'gleaming', 'withholding',
+  'bulbous', 'brittle']
+const nouns = ['kitten', 'mountain', 'pillow', 'ice cream cone', 'tulip', 'lake', 'fortress', 'lemon',
+  'popsicle', 'librarian', 'python', 'pony', 'melon', 'bedsheet', 'muffin', 'bunny rabbit',
+  'fish', 'princess', 'ghost', 'waterfall', 'mango', 'harpy', 'ship', 'nun',
+  'berry', 'car']
+const verbs1 = ['longed', 'lusted', 'wished', 'wanted', 'resolved', 'dared', 'detested', 'trembled',
+  'thirsted', 'expected', 'planned', 'deigned', 'proposed', 'shuddered', 'refused', 'hated',
+  'scorned', 'dreaded', 'did not care', 'ached', 'intended', 'hungered', 'craved', 'hankered',
+  'needed', 'pined']
+const verbs2 = ['ravish', 'climb', 'invade', 'grope', 'marry', 'raw dog it with', 'cares', 'proposition',
+  'correct', 'emotionally manipulate', 'spar with', 'compliment', 'hire', 'booty call', 'mansplain to', 'insult',
+  'date', 'teabag', 'ignore', 'fondle', 'worship', 'examine', 'manhandle', 'touch',
+  'admire', 'demand things from']
 
 const maleAuthorDict = [attr, adj, nouns, verbs1, verbs2]
 
